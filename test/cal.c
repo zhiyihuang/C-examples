eaadc7b76c88e49761176e00a88eb5e83f4d1bb1
Ruth-Huang <rmhhuang@gmail.com>
2019-10-20 01:27:50 +0000

    done with cal.c

--- test/cal.c
+++ test/cal.c
@@ -1,5 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+char *get_token(char *s)
+{
+int i;
+char *token;
+token = malloc(100);
+i = 0;
+while(s[i] != ' ' && s[i] != 0){
+token[i] = s[i];
+i = i + 1;
+}
+token[i] = 0;
+return token;
+}
+
+
+int is_operator(char *s)
+{
+if(strlen(s) != 1) return 0;
+if(s[0] == '+' || s[0] == '-' || s[0] == '*') return 1;
+else return 0;
+}
+
+int is_number(char *s)
+{
+int i;
+i = 0;
+while(s[i] != 0){
+    if(s[i] >= '0' && s[i] <= '9') i = i+1;
+    else return 0;
+}
+return 1;
+}
+
 int addition(int x, int y)
 {
 return x+y;
@@ -16,10 +50,16 @@ return x-y;
 }
 int main(int argc, char *argv[])
 {
-if(argc != 3) {
-printf("Usage: %s num1 num2\n", argv[0]);
+if(argc != 2) {
+printf("Usage: %s \n", argv[0]);
 exit(0);
 }
+if(is_number(argv[1])) printf("yes %s is a number\n", argv[1]);
+else printf("No, %s is not a number\n", argv[1]);
+
+exit(0);
+
+
 int num1, num2;
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
