906189eeb55876a685964e402d69f94696f3c97d
Ruth-Huang <rmhhuang@gmail.com>
2019-11-04 00:42:26 +0000

    update cal.c

--- test/cal.c
+++ test/cal.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-char *get_token(char *s)
+char *get_token(char *s, char *token)
 {
-int i;
-char *token;
-token = malloc(100);
-i = 0;
-while(s[i] != ' ' && s[i] != 0){
-token[i] = s[i];
+int i, j;
+
+i = 0; j = 0;
+while(s[i] == ' ') i = i + 1;
+while(s[i] != ' ' && s[i] != 0 && s[i] != '\n'){
+token[j] = s[i];
 i = i + 1;
+j = j + 1;
 }
-token[i] = 0;
-return token;
+token[j] = 0;
+return s+i;
 }
 
 
@@ -50,46 +51,48 @@ return x-y;
 }
 int main(int argc, char *argv[])
 {
-if(argc != 2) {
+char buf[100];
+if(argc != 1) {
 printf("Usage: %s \n", argv[0]);
 exit(0);
 }
-if(is_number(argv[1])) printf("yes %s is a number\n", argv[1]);
-else printf("No, %s is not a number\n", argv[1]);
 
-exit(0);
+fgets(buf, 100, stdin);
+printf("The string is %s\n", buf);
+printf("The length of the string is %ld\n", strlen(buf));
+char num1[100], num2[100], op[100], *p;
+p = get_token(buf, num1);
+if(is_number(num1)==0) {
+	printf("Wrong number: %s\n", num1);
+	exit(0);
+}
+p = get_token(p, op);
+if(is_operator(op)==0) {
+	printf("Wrong operator: %s\n", op);
+	exit(0);
+}
+p = get_token(p, num2);
+if(is_number(num2)==0) {
+	printf("Wrong number: %s\n", num2);
+	exit(0);
+}
 
-
-int num1, num2;
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
-while(1){
-printf("choose one of the following operations\n");
-printf("a: add\n");
-printf("m: multiply\n");
-printf("s: subtract\n");
-printf("q: quit!\n");
-char c = getchar();
-if(c=='q') exit(0);
-getchar();
-printf("input the first operand:");
-scanf("%d", &num1);
-printf("input the second operand:");
-scanf("%d", &num2);
-int r;
-if(c=='a') {
-r = addition(num1, num2);
+int n1, n2, r;
+n1 = atoi(num1);
+n2 = atoi(num2);
+char c;
+c = op[0];
+if(c=='+') {
+r = addition(n1, n2);
 printf("The result is %d\n", r);
 }
-if(c=='m') {
-r = multiply(num1, num2);
+if(c=='*') {
+r = multiply(n1, n2);
 printf("The multiplication is %d\n", r);
 }
-if(c=='s'){
-r = subtract(num1, num2);
+if(c=='-'){
+r = subtract(n1, n2);
 printf("The subtraction is %d\n", r);
-}
-getchar();
 }
 }
 
